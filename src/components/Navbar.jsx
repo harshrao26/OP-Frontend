46d92c989b2193b2489066502bcfa3746d1c3dbd
Harsh D. Rao <harshrao26>
2025-03-28 18:19:20 +0530

    Search Bar UI Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
         </Link>
 
         {/* Search Bar - Hidden on Small Screens */}
-        <div className="md:w-full w-60  relative ">
+        <div className="md:w-1/2 w-60  relative ">
           <div className="flex items-center bg-gray-100 px-3 py-2 rounded-md">
             <Search className="w-5 h-5 text-gray-500" />
             <input
